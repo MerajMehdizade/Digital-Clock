0da58fea8948f76650f7927547c486b46c4a6f40
MerajMehdizade <mdzmeraj8@gmail.com>
2023-09-21 02:05:22 +0430

    add hours article

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-gray": "#303640",
+        seconds: "rgba(6, 252, 63, 1)",
+        minutes: "rgba(252, 230, 0, 1)",
+        hours: "rgba(253, 41, 112, 1)",
+      },
+      fontFamily: {
+        body: ["Ubuntu", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
